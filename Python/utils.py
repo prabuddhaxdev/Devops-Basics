70238c707a7455d6f5decf680b6e5fc41c0aa9dc
prabuddhaxdev <prabuddha1749@gmail.com>
2026-01-14 12:02:33 +0530

    Add python utilities

---
+++ Python/utils.py
@@ -0,0 +1,6 @@
+# For Linux/Unix or Mac only :
+import os
+
+print(os.system('df -h'))  # system information
+print(os.system('uptime'))  # uptime and load average
+print(os.system('free -h')) # RAM
